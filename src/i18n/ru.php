51963ad0d6ec45f1f1f31a3eaf35fb6935851131
Alexander Kuzmin <roosit@abricos.org>
2014-11-06 20:19:34 +0300

    i18n: roles

--- src/i18n/ru.php
+++ src/i18n/ru.php
@@ -1,6 +1,11 @@
 <?php
 return array(
     "title" => "Дела",
+    'roles' => array(
+        '10' => 'Чтение',
+        '30' => 'Запись',
+        '50' => 'Администрирование'
+    ),
     "bosmenu" => array(
         "todolist" => "Дела"
     )
